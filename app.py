efeb9e884b974b3760953ffd6b0a646acd03fe7b
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2023-06-19 02:09:50 +0000

    add function for art generator.

--- app.py
+++ app.py
@@ -22,6 +22,24 @@ def index():
     return render_template("index.html", result=result)
 
 
+@app.route("/image", methods=("GET", "POST"))
+def image():
+    image = ""
+    if request.method == "POST":
+        image = request.form.get("image", False)
+        response = openai.Image.create(
+            prompt=image,
+            n = 1,
+            size = "512x512"
+        )
+        image_url = response['data'][0]['url']
+        return redirect(url_for("image", result=image_url, prompt=image))
+
+    result = request.args.get("result")
+    image = request.args.get("prompt", "")
+    return render_template("image.html", result=result, prompt=image)
+
+
 def generate_prompt(animal):
     return """Suggest three names for an animal that is a superhero.
 
